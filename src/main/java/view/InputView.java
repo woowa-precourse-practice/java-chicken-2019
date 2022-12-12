bc14e71202ea2d4d3ae29bab3659668e348a01ab
khj1 <dhepd8849@gmail.com>
2022-12-12 11:45:10 +0900

    feat: 원하는 기능을 입력 받는 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,12 +1,33 @@
 package view;
 
+import command.MainCommand;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class InputView {
+    private static final String MAIN_COMMAND_IS = "## 메인화면";
+    private static final String REQUEST_MAIN_COMMAND = "## 원하는 기능을 선택하세요.";
+    private static final String REQUEST_TABLE_FOR_ORDER = "## 주문할 테이블을 선택하세요.";
+
     private static final Scanner scanner = new Scanner(System.in);
 
+    public static MainCommand inputMainCommand() {
+        printMain();
+
+        System.out.println(REQUEST_MAIN_COMMAND);
+        return MainCommand.from(scanner.nextInt());
+    }
+
+    private static void printMain() {
+        System.out.println(MAIN_COMMAND_IS);
+        Arrays.stream(MainCommand.values())
+                .forEach(System.out::println);
+        System.out.println();
+    }
+
     public static int inputTableNumber() {
-        System.out.println("## 주문할 테이블을 선택하세요.");
+        System.out.println(REQUEST_TABLE_FOR_ORDER);
         return scanner.nextInt();
     }
 }
